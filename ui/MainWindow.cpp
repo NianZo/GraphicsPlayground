bcc59ce2908296e05c603ec890f3af5c502ded75
NianZo <nicholas.angle@gmail.com>
2022-10-14 20:52:39 -0700

    Add back previous code to MainWindow constructor

--- ui/MainWindow.cpp
+++ ui/MainWindow.cpp
@@ -18,27 +18,27 @@ MainWindow::MainWindow() : //: m_window(w)
   ui->setupUi(this);
 
   appObj = VulkanApplication::GetInstance();
-//
-//  inst.setVkInstance(appObj->instanceObj.instance);
-//
-//  if (!inst.create())
-//    qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
-//
-//  m_window = new QWindow;
-//  m_window->setSurfaceType(QSurface::VulkanSurface);
-//  m_window->setVulkanInstance(&inst);
-//
-//  m_windowWrapper = QWidget::createWindowContainer(m_window, ui->widget);
-//  m_windowWrapper->setMinimumSize(ui->widget->size());
 
-//  show();
-//  surface = QVulkanInstance::surfaceForWindow(m_window);
-//  if (surface == VK_NULL_HANDLE) {
-//    std::cout << "Got NULL surface from surfaceForWindow\n";
-//  }
+  inst.setVkInstance(appObj->instanceObj.instance);
 
-  //appObj->initialize(&surface, (uint32_t)m_window->width(), (uint32_t)m_window->height());
-  //appObj->prepare();
+  if (!inst.create())
+    qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
+
+  m_window = new QWindow;
+  m_window->setSurfaceType(QSurface::VulkanSurface);
+  m_window->setVulkanInstance(&inst);
+
+  m_windowWrapper = QWidget::createWindowContainer(m_window, ui->widget);
+  m_windowWrapper->setMinimumSize(ui->widget->size());
+
+  show();
+  surface = QVulkanInstance::surfaceForWindow(m_window);
+  if (surface == VK_NULL_HANDLE) {
+    std::cout << "Got NULL surface from surfaceForWindow\n";
+  }
+
+  appObj->initialize(&surface, (uint32_t)m_window->width(), (uint32_t)m_window->height());
+  appObj->prepare();
   std::cout << "Finished MainWindow::MainWindow()\n";
 
 }
@@ -46,8 +46,8 @@ MainWindow::MainWindow() : //: m_window(w)
 MainWindow::~MainWindow()
 {
 
-	//close(); // needed or there will be a segfault
-	//delete m_window;
+	close(); // needed or there will be a segfault
+	delete m_window;
 	//delete m_window;
 	std::cout << "Called delete m_window\n";
 	delete ui;
