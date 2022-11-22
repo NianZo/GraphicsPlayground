2962eb8570cc0fa5dfdd7a07a654426cd23a1950
NianZo <nicholas.angle@gmail.com>
2022-11-21 18:00:04 -0800

    Prepare Docker assets for upload to Dockerhub

--- .github/actions/Docker-CI/DockerEntrypoint.sh
+++ .github/actions/Docker-CI/DockerEntrypoint.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
+# Inputs:
+# $1: The command to run (build&test, format, or tidy)
+# $2: A codecov.io upload token passed in as a secret from the project settings on Github
 echo "Running DockerEntrypoint.sh"
 echo "Command: $1"
 if [ $1 = "build&test" ]
 then
     cd src
-    #startx&
+    # Start a virtual display for GUI apps
     Xvfb :1 -screen 0 1920x1080x16&
     export DISPLAY=:1.0
-    #export QT_QPA_PLATFORM=vkkhrdisplay
-    #export LIBGL_ALWAYS_SOFTWARE=true
-    #export GALLIUM_DRIVER=llvmpipe
+
+    # Build project
     ./compile_shaders.sh
     cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
     cmake --build build --config Debug
+
+    # Required for code coverage in Clang
     export LLVM_PROFILE_FILE=/profile.profraw
 
-    #cd build
-    #ctest -C Debug --output-on-failure
     ctest --test-dir build -C Debug --output-on-failure
 
+    # If ctest failed then return an error here
     if [ $? != 0 ]
     then
         exit 1
     fi
 
+    # Calculate code coverage and report it to codecov.io
     llvm-profdata merge -sparse /profile.profraw -o build/coverage.profdata
     llvm-cov show build/test/UIUnitTests -instr-profile=build/coverage.profdata > build/coverage.txt
-    #cd ..
-    #bash <(curl -S https://codecov.io/bash)
     curl -Os https://uploader.codecov.io/latest/linux/codecov
     chmod +x codecov
     ./codecov -t $2
@@ -42,14 +44,12 @@ then
     cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
     cmake --build build --config Debug
     clang-tidy -p build ui/*pp
-elif [ $1 = "interactive" ]
-then
-    /bin/bash
 fi
 
-# to run locally: 
+# to run locally run following commands from the directory holding this file: 
 # docker build -t qttest -f Dockerfile .
-# docker run --name qttest-container --privileged -v "$(pwd)"/../../../:/src qttest "build&test"
+# docker run --name qttest-container -v "$(pwd)"/../../../:/src qttest "build&test"
+# docker run -it --name qttest-container -v "$(pwd)"/../../../:/src --entrypoint /bin/bash qttest (for interactive mode)
 # to remove after running: 
 # docker rm qttest-container
 # docker image rm qttest
